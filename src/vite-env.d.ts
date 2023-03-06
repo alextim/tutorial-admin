354c143e4993ad82c4bb15745ff43124b757681f
Alex Tim <buildmnt@gmail.com>
2023-03-06 16:40:14 +0200

    fix: typings

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_GOOGLE_CLIENT_ID: string;
   readonly VITE_API_URL: string;
   readonly VITE_USER_KEY: string;
+  readonly VITE_UPLOADS_URL: string;
   // more env variables...
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,24 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig, loadEnv  } from 'vite';
+// import react from '@vitejs/plugin-react-swc';
+import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  server: {
-    proxy: {
-      '/uploads': {
-        target: 'http://localhost:4000',
-        changeOrigin: true,
-        secure: false,
-        // rewrite: (path) => path.replace(/^\/uploads/, "/uploads"),
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  return defineConfig({
+    server: {
+      proxy: {
+        '/uploads': {
+          target: process.env.VITE_UPLOADS_URL,
+          changeOrigin: true,
+          secure: false,
+          // rewrite: (path) => path.replace(/^\/uploads/, "/uploads"),
+        },
       },
     },
-  },
-  build: {
-    minify: false,
-  },
-  plugins: [react()],
-});
+    build: {
+      minify: false,
+    },
+    plugins: [react()],
+  });
+};
