164db4728b4033dbe52e829c236df31ab7c478d1
Alex Tim <buildmnt@gmail.com>
2023-03-06 16:09:32 +0200

    fix: typings

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_GOOGLE_CLIENT_ID: string;
+  readonly VITE_API_URL: string;
+  readonly VITE_USER_KEY: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
